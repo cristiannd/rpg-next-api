cf9179b885501fc000e6090704e888e929bbb0c1
cristiannd <cristian.donalicio@gmail.com>
2024-02-01 11:06:37 -0300

    init project

---
+++ src/main/java/com/cris/rpgnext/RpgNextApplication.java
@@ -0,0 +1,13 @@
+package com.cris.rpgnext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgNextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgNextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cris/rpgnext/RpgNextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cris.rpgnext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpgNextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
