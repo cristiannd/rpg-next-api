6ba709992f568f231f36e49375f5480f878bba66
cristiannd <cristian.donalicio@gmail.com>
2024-02-03 11:50:35 -0300

    Refactor services

--- src/main/java/com/cris/rpgnext/service/Impl/LevelService.java
+++ src/main/java/com/cris/rpgnext/service/Impl/LevelService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class LevelService implements ILevelService {
+
   @Autowired
   LevelRepository levelRepository;
 
--- src/main/java/com/cris/rpgnext/service/Impl/QuestService.java
+++ src/main/java/com/cris/rpgnext/service/Impl/QuestService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class QuestService implements IQuestService {
+
   @Autowired
   private QuestRepository questRepository;
 
