26554878e238bfacb950e249a4ec30058f003be9
cristiannd <cristian.donalicio@gmail.com>
2024-02-05 13:53:58 -0300

    Add WeaponCategory and relation with Weapon

--- src/main/java/com/cris/rpgnext/entity/WeaponCategory.java
+++ src/main/java/com/cris/rpgnext/entity/WeaponCategory.java
@@ -6,23 +6,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 @Entity
-@Table(name = "weapons")
+@Table
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class Weapon {
+public class WeaponCategory {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
-  private Integer score;
-  private Boolean oneHand;
-  private Integer physicalDamage;
-  private Integer actualDurability;
-  private Integer maxDurability;
-  @ManyToOne
-  @JoinColumn(name = "weapon_category_id")
-  private WeaponCategory weaponCategory;
+  @OneToMany(mappedBy = "weaponCategory")
+  private List<Weapon> weapons = new ArrayList<>();
 }
