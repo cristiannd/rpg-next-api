43812e08f3acac29665f867c0f3b98b8fc496fee
cristiannd <cristian.donalicio@gmail.com>
2024-02-05 13:06:47 -0300

    Add relation between Item and ItemCategory

--- src/main/java/com/cris/rpgnext/entity/Item.java
+++ src/main/java/com/cris/rpgnext/entity/Item.java
@@ -1,13 +1,24 @@
 package com.cris.rpgnext.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "items")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Item {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private Integer score;
   private String name;
+  @ManyToOne
+  @JoinColumn(name = "item_category_id")
+  private ItemCategory itemCategory;
 }
--- src/main/java/com/cris/rpgnext/entity/ItemCategory.java
+++ src/main/java/com/cris/rpgnext/entity/ItemCategory.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 @Table(name = "item_categories")
 @AllArgsConstructor
@@ -17,4 +20,6 @@ public class ItemCategory {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
+  @OneToMany(mappedBy = "itemCategory")
+  private Set<Item> items = new HashSet<>();
 }
